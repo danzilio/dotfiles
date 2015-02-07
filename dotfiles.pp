26e67265cb25d6d071c4d63bae7b918872bc9021
David Danzilio <david.danzilio@mac.com>
2015-02-07 12:09:49 -0500

    Adding gemrc and atom configuration. Added puppet manifest to set up the links

---
+++ dotfiles.pp
@@ -0,0 +1,36 @@
+# Sets up my dotfiles. Requires the future parser.
+#
+class dotfiles {
+  $home = inline_template('<%= Dir.home %>')
+
+  vcsrepo { "${home}/.oh-my-zsh":
+    ensure     => present,
+    provider   => git,
+    submodules => true,
+    source     => 'https://github.com/robbyrussell/oh-my-zsh.git',
+  }
+
+  vcsrepo { "${home}/.dotfiles":
+    ensure     => present,
+    provider   => git,
+    submodules => true,
+    source     => 'https://github.com/danzilio/dotfiles.git',
+  }
+
+  $links = {
+    "${home}/.gitconfig" => "${home}/.dotfiles/gitconfig",
+    "${home}/.vim" => "${home}/.dotfiles/vim",
+    "${home}/.vimrc" => "${home}/.vim/vimrc",
+    "${home}/.zsh_aliases" => "${home}/.dotfiles/aliases",
+    "${home}/.zshrc" => "${home}/.dotfiles/zshrc",
+    "${home}/.atom" => "${home}/.dotfiles/atom",
+    "${home}/.gemrc" => "${home}/.dotfiles/gemrc",
+  }
+
+  $links.each |$link,$target| {
+    file { $link:
+      ensure => link,
+      target => $target
+    }
+  }
+}
